03e30a76c246476eb333ef878c439f4c40740f07
Arcady Usov <itcrab@ya.ru>
2015-03-02 21:32:30 +0500

    Install GulpJS and add split tasks for CSS&JS with exec scripts.

---
+++ exec/gulp_compile.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd ..
+node_modules\.bin\gulp.cmd compile
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp       = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat     = require('gulp-concat'),
+    watch      = require('gulp-watch');
+
+// CSS
+gulp.task('css', function () {
+    return gulp.src([
+        './bower_components/bootstrap/dist/css/bootstrap.min.css',
+        './css/main.css'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.min.css'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./css/min'));
+});
+
+// JS
+gulp.task('js', function () {
+    return gulp.src([
+        './bower_components/jquery/dist/jquery.min.js',
+        './bower_components/jquery-migrate/jquery-migrate.min.js',
+        './bower_components/bootstrap/dist/js/bootstrap.min.js',
+        './js/main.js'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.min.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./js/min'));
+});
+
+// JS for IE
+gulp.task('js-ie', function () {
+    return gulp.src([
+        './bower_components/html5shiv/dist/html5shiv.min.js',
+        './bower_components/respond/dest/respond.min.js'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.ie.min.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./js/min'));
+});
+
+// Watcher
+gulp.task('watch', function () {
+    gulp.watch([
+        './bower_components/**/*.css',
+        './css/main.css'
+        ], ['css']);
+    gulp.watch([
+        './bower_components/**/*.js',
+        './js/main.js'
+        ], ['js', 'js-ie']);
+});
+
+// Compile
+gulp.task('compile', ['css', 'js', 'js-ie']);
+
+// default
+gulp.task('default', ['watch']);
